90baae0c2faf7b069df2fc78a180059cd7bdb87a
RealSput <dmtr.jms420@gmail.com>
2025-07-16 22:18:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# g.js-binary
+A toolkit for G.js that allows you to do bit-level manipulation and reading (binary conversion to triggers, bitwise operators, 2^x exponentiation, etc.)
+
+# Example
+```js
+import '@g-js-api/g.js';
+import Binary from '@g-js-api/binary'
+
+await $.exportConfig({
+    type: 'live_editor',
+    options: { info: true }
+});
+
+let binary = new Binary(16); // MUST be run before anything else!
+
+let blockk = unknown_g();
+object({
+    OBJ_ID: 1,
+    X: 15,
+    Y: -75,
+    GROUPS: block
+}).add();
+
+// does 2 ^ 5
+let res1 = binary.pow2(5);
+res1.display(45, -15);
+
+// moves `mgroup` by 32 (2 ^ 5) steps
+binary.convert(res1, (exp) => {
+  mgroup.move(exp, 0);
+})
+
+// does `res1 << 2`
+let res2 = binary.bitwise(res1, LSHIFT, 2);
+res2.display(75, -15);
+```
+
+# Docs
+Class `Binary`:
+- Argument to constructor: the maximum amount of bits that will be used
+- Usage: `new Binary(16)`
+
+Methods:
+- `.bitwise`: Does a bitwise operation on two values (one for NOT)
+    - Parameters:
+        - value1 (number / counter): The first value to the operation
+        - op (number): The bitwise operator (AND, OR, XOR, LSHIFT, RSHIFT, NAND, NOR, NOT, XNOR)
+        - value2 (number / counter): The second value to the operation (optional only for NOT operator)
+        - copy (boolean, optional): Whether to copy to a new counter or to directly return the counter used in the backend
+- `.pow2`: Calculates 2^x
+    - Parameters:
+        - exp (number): The exponent to base 2
+        - copy (boolean, optional): Whether to copy to a new counter or to directly return the counter used in the backend
+- `convert`: Converts a counter to triggers by repeatedly subtracting powers of two (only works on additive triggers, e.g. move, rotate, scale)
+    - Parameters:
+        - value (counter): The counter to convert into triggers
+        - fn (function): The function that holds the triggers
+            - Parameters passed to function: `exp`, which stores the current power of two
---
+++ index.js
@@ -0,0 +1,164 @@
+extract({
+    AND: 1,
+    OR: 2,
+    XOR: 3,
+    LSHIFT: 4,
+    RSHIFT: 5,
+    NAND: 6,
+    NOR: 7,
+    NOT: 8,
+    XNOR: 9
+});
+
+let init = (bits) => {
+    if (typeof bits !== "number") throw `Expected number for 'bits', got ${typeof bits}`;
+    bits = Math.round(Math.abs(bits));
+    if (bits > 31) throw "Geometry Dash only supports 32-bit SIGNED integers!";
+
+    let b = counter(2);
+    let cresult = counter(1);
+
+    let cexp = (exp, copy = true) => {
+        cresult.set(1);
+        b.set(2);
+
+        let e = float_counter().set(exp);
+        let ctxn;
+
+        wait(.01);
+        trigger_function(() => {
+            let ctx = $.trigger_fn_context();
+            compare(e, GREATER, 0, trigger_function(() => {
+                item_comp(e.item, e.item, TIMER, TIMER, NOT_EQ, trigger_function(() => {
+                    cresult.multiply(b);
+                }), undefined, 2, 2, undefined, DIV, DIV, NONE, NONE, FLR, RND).add();
+                wait(.01);
+                b.multiply(b);
+                item_edit(e.item, undefined, e.item, TIMER, NONE, TIMER, EQ, DIV, DIV, 2, undefined, NONE, FLR).add();
+                ctx.call(.01);
+            }), trigger_function(() => {
+                ctxn = $.trigger_fn_context();
+            }));
+        }).call();
+        Context.set(ctxn);
+        return copy ? counter().set(cresult) : cresult;
+    }
+
+    let val1 = counter(),
+        val2 = counter(),
+        result = counter(),
+        mode = counter();
+
+    let bitwiseF = trigger_function(() => {
+        let bit1 = counter();
+        let bit2 = counter();
+        result.reset();
+
+        let reset = trigger_function(() => {
+            bit1.set(0);
+            bit2.set(0);
+        })
+
+        let expr = cexp(val2, false);
+
+        compare(mode, EQ, 4, trigger_function(() => {
+            // LSHIFT
+            result.set(val1).multiply(expr);
+        }), trigger_function(() => {
+            compare(mode, EQ, 5, trigger_function(() => {
+                // RSHIFT
+                item_edit(val1.item, expr.item, result.item, ITEM, ITEM, ITEM, EQ, DIV, undefined, undefined, undefined, undefined, FLR).add();
+            }), trigger_function(() => {
+                // ALL BITWISE
+                for (let i = bits - 1; i >= 0; i--) {
+                    reset.call();
+
+                    let exp = 2 ** i;
+                    compare(val1, GREATER_OR_EQ, exp, trigger_function(() => {
+                        val1.subtract(exp);
+                        bit1.set(1);
+                    }));
+                    compare(val2, GREATER_OR_EQ, exp, trigger_function(() => {
+                        val2.subtract(exp);
+                        bit2.set(1);
+                    }));
+
+                    // BITWISE OPS
+                    // AND
+                    compare(mode, EQ, 1, trigger_function(() => {
+                        bit1.multiply(bit2);
+                        compare(bit1, EQ, 1, trigger_function(() => result.add(exp)));
+                    }))
+                    // OR
+                    compare(mode, EQ, 2, trigger_function(() => {
+                        bit1.add(bit2);
+                        compare(bit1, GREATER, 0, trigger_function(() => result.add(exp)));
+                    }))
+                    // XOR
+                    compare(mode, EQ, 3, trigger_function(() => {
+                        compare(bit1, NOT_EQ, bit2, trigger_function(() => result.add(exp)));
+                    }))
+                    // NAND
+                    compare(mode, EQ, 6, trigger_function(() => {
+                        bit1.multiply(bit2);
+                        compare(bit1, EQ, 0, trigger_function(() => result.add(exp)));
+                    }));
+                    // NOR
+                    compare(mode, EQ, 7, trigger_function(() => {
+                        bit1.add(bit2);
+                        compare(bit1, EQ, 0, trigger_function(() => result.add(exp)));
+                    }))
+                    // NOT
+                    compare(mode, EQ, 8, trigger_function(() => {
+                        compare(bit1, EQ, 0, trigger_function(() => result.add(exp)));
+                    }))
+                    // XNOR
+                    compare(mode, EQ, 9, trigger_function(() => {
+                        compare(bit1, EQ, bit2, trigger_function(() => result.add(exp)));
+                    }));
+                }
+            }))
+        }))
+
+        group(9998).call();
+    });
+
+    let bitwise = (v1, op, v2 = 0, copy = true) => {
+        val1.set(v1);
+        mode.set(op);
+        val2.set(v2);
+        let ctx = trigger_function(() => {});
+        bitwiseF.remap([9998, ctx]).call();
+        Context.set(ctx);
+        return copy ? counter().set(result) : result;
+    }
+
+    let convert = (val, fn) => {
+        let vcopy = counter().set(val);
+        for (let i = bit - 1; i >= 0; i--) {
+            let exp = 2 ** i;
+            compare(vcopy, GREATER_OR_EQ, exp, trigger_function(() => {
+                vcopy.subtract(exp);
+                fn(exp);
+            }));
+        }
+    };
+
+    return {
+        bitwise,
+        convert,
+        pow2: cexp
+    }
+};
+
+// todo: make it a TRUE class later on
+// just did this cuz i got lazy lmfao
+
+class Binary {
+    constructor(bits) {
+        let instance = init(bits);
+        Object.assign(this, instance);
+    }
+}
+
+export default Binary;
